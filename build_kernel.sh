45b19972e6738fa4e58e4799fcc34762f371cf2d
tao1996 <39122705+tao1996@users.noreply.github.com>
2020-11-23 20:49:39 +0800

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,16 +8,8 @@ KERNEL_DEFCONFIG=sdm660_perf_defconfig
 echo "|| Cloning GCC ||"
 mkdir gcc
 cd gcc
-mkdir arm32-gcc
-cd arm32-gcc
-git clone -b eleven https://github.com/AOSiP/platform_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 --depth 1
-cd ..
-mkdir arm64-gcc
-cd arm64-gcc
-git clone -b eleven https://github.com/AOSiP/platform_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 --depth 1
-cd ..
-echo "|| Cloning KERNEL ||"
-cd gcc
+git clone -b hmp https://github.com/vijaymalav564/lockdown_kernel_realme2pro
+
 
 tar -cvpzf /home/runner/gcc32.tar.gz ./
 
