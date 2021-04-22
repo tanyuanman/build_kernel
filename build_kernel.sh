e5f2ec876ad7f7520939755b85c516e9ab4a17ef
tanyuanman <70801810+tanyuanman@users.noreply.github.com>
2021-04-22 19:45:34 +0800

    Create build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #内核源码地址
-KERNEL_URL=https://github.com/oppo-source/R15Pro-9.0-kernel-source
+KERNEL_URL=https://github.com/tanyuanman/android_kernel_xiaomi_mt6765.git
 #内核defconfig
-KERNEL_DEFCONFIG=sdm660_perf_defconfig
+KERNEL_DEFCONFIG=cactus_defconfig
 
 #谷歌GCC4.9
 echo "|| Cloning GCC ||"
