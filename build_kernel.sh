5941e1805121fcb89b408e8926c688bed8b7c123
tanyuanman <70801810+tanyuanman@users.noreply.github.com>
2021-04-22 20:04:00 +0800

    Create build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,6 +4,10 @@
 git clone https://github.com/tanyuanman/android_kernel_xiaomi_mt6765.git -b oreo
 git clone git clone git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 gcc
 
-
+export CROSS_COMPILE=gcc/bin/arm-linux-androideabi-
+export ARCH=arm && export SUBARCH=arm
+mkdir out
+make o=out cactus_defconfig 
+make o=out -j$(nproc --all)
 
 
