d97f012afb4d242134c79493399bfbd6718a4d41
tanyuanman <70801810+tanyuanman@users.noreply.github.com>
2021-04-22 19:54:21 +0800

    Create build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,18 +2,8 @@
 
 
 git clone https://github.com/tanyuanman/android_kernel_xiaomi_mt6765.git -b oreo
-git clone git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 gcc
-KERNEL_DEFCONFIG=cactus_defconfig
+git clone git clone git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 gcc
 
-#谷歌GCC4.9
-echo "|| Cloning GCC ||"
 
-git clone -b master https://github.com/redfeast/android_kernel_oppo_RMX1801 gcc
 
-cd ./gcc
-tar -cvpzf /home/runner/gcc32.tar.gz ./
 
-#
-echo
-echo "Done!"
-echo
